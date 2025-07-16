775e22e6204b4929edfd6a55cf97f1e36977f30d
Dominique Boeckli <dboeckli@gmail.com>
2025-07-16 18:46:45 +0200

    fix unit tests 2

--- src/test/java/guru/springframework/joke/jokeapp/service/JokeServiceImplTest.java
+++ src/test/java/guru/springframework/joke/jokeapp/service/JokeServiceImplTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.joke.jokeapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,9 +11,24 @@ class JokeServiceImplTest {
     @Test
     void testJoke() {
         ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
-        assertThat(chuckNorrisQuotes.getRandomQuote())
+        String joke = chuckNorrisQuotes.getRandomQuote();
+        assertThat(joke)
             .isNotNull()
-            .containsIgnoringCase("Chuck")
-            .containsIgnoringCase("Norris");
+            .satisfiesAnyOf(
+                j -> assertThat(j).containsIgnoringCase("Chuck").containsIgnoringCase("Norris"),
+                j -> assertThat(j).containsIgnoringCase("Superman")
+            );
+    }
+
+    @RepeatedTest(200)
+    void testJokeRepeated() {
+        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+        String joke = chuckNorrisQuotes.getRandomQuote();
+        assertThat(joke)
+            .isNotNull()
+            .satisfiesAnyOf(
+                j -> assertThat(j).containsIgnoringCase("Chuck").containsIgnoringCase("Norris"),
+                j -> assertThat(j).containsIgnoringCase("Superman")
+            );
     }
 }
